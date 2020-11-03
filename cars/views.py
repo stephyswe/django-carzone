3d12bfd686dbd72472ba4f7a44c0085c819aa725
Stephanie <stephaniiehallberg@gmail.com>
2020-11-03 14:52:31 +0100

    Pagination in Car Page

--- cars/views.py
+++ cars/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Car
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 def cars(request):
-    cars = Car.objects.order_by('created_date')
+    cars = Car.objects.order_by('-created_date')
+    paginator = Paginator(cars, 1)
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
     data = {
-        'cars': cars
+        'cars': paged_cars
     }
     return render(request, 'cars/cars.html', data)
 
