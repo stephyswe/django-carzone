72439e18ff1f3b41fd41ffef1f57099146e29ee2
Stephanie <secret@gmail.com>
2020-11-05 10:01:13 +0100

    Registration Form Setup - CSRF Token

--- accounts/views.py
+++ accounts/views.py
@@ -8,7 +8,11 @@ def login(request):
     return render(request, 'accounts/login.html')
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        print('this is post method')
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
