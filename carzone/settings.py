4ff0de6ad7e2cb2b59b0c2f2c6d1f4d93d811e51
Stephanie <secret@gmail.com>
2020-11-07 10:16:38 +0100

    Login with Facebook

--- carzone/settings.py
+++ carzone/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_REDIRECT_URL = 'dashboard'
+
 
 # Application definition
 
@@ -42,6 +44,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ckeditor',
     'django.contrib.humanize',
+    'django.contrib.sites',
+    'allauth.account',
+    'allauth.socialaccount',
+    
+    #Providers
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -145,3 +154,6 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# Social Login
+SITE_ID = 1
