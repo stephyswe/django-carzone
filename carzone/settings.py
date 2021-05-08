2a251ae5df2683b041c736db18d8d5801f2509fa
Stephaniie <stephaniiehallberg@gmail.com>
2021-05-08 02:04:45 +0200

    added whitenoise config

--- carzone/settings.py
+++ carzone/settings.py
@@ -144,6 +144,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
